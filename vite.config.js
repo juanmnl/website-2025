969f7c7560ca582365333a188e998c1c1b609d22
juanmnl <juanmnlcornejo@gmail.com>
2025-06-15 17:38:36 -0500

    Update vite config to fix build error

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
-import { viteImageOptimize } from 'vite-plugin-imagemin';
+import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
   plugins: [
-    viteImageOptimize({
+    viteImagemin({
       gifsicle: { optimizationLevel: 7 },
       mozjpeg: { quality: 80 },
       pngquant: { quality: [0.65, 0.8] },
